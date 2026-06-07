7ea7351449c349e9dc797beecebf6d9562d63d6d
Kirill Byvshev <k@byv.sh>
2026-06-07 17:28:08 +0400

    Add entrypoint readiness marker (#37)

--- open.sh
+++ open.sh
@@ -31,7 +31,7 @@ run_worker() {
     printf '\033]1337;SetUserVar=CLAUDEW=%s\007' "$(printf '%s' "$name" | base64)"
     trap 'printf "\033]1337;SetColors=tab=\007\033]1337;SetColors=cursor=\007\033]1337;SetColors=bg=\007\033]0;\007\033]1337;SetUserVar=CLAUDEW=\007"' EXIT
 
-    docker compose up -d --build "$service"
+    docker compose up -d --build --wait "$service"
     docker compose exec "$service" claudew
 }
 
